f22d0170e9a9721db92d4d050040579f984e1581
narojunagaraju <narojunagarajun@gmail.com>
2024-04-05 17:55:15 +0530

    icon code

--- lib/main.dart
+++ lib/main.dart
@@ -15,26 +15,11 @@ class Home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      /* use it for text
       body: Center(
-          child: Text(
-        'Hello Ninjas',
-        style: TextStyle(
-            fontSize: 20.0,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'),
-      )),*/
-      /* Use it for NetWork Image
-      body: Center(
-        child: Image(
-          image: NetworkImage('https://plus.unsplash.com/premium_photo-1661877737564-3dfd7282efcb?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
-        ),
-      ),*/
-      body: Center(
-        child: Image(
-          image: AssetImage('assets/image1.jpg'),
+        child: Icon(
+          Icons.airport_shuttle,
+          color: Colors.lightBlue,
+          size: 50.0,
         ),
       ),
       floatingActionButton: FloatingActionButton(
